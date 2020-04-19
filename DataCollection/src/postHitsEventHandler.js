8dc83704aebe8acea5696c904015de093c2e479d
Simon Angell <simon.angell307@gmail.com>
2020-04-19 16:35:23 +1000

    Clean data and fix data collection bugs

--- DataCollection/src/postHitsEventHandler.js
+++ DataCollection/src/postHitsEventHandler.js
@@ -5,20 +5,70 @@ const AWS = require('aws-sdk')
 const documentClient = new AWS.DynamoDB.DocumentClient({ apiVersion: '2012-10-08' })
 const eventTableName = 'hitsEventData'
 
+function getNewEventPhase(phase) {
+    if (phase === 'propose') {
+        return 'failedVote'
+    }
+
+    if (phase === 'check_outcome') {
+        return 'missionOutcome'
+    }
+
+    return phase
+}
+
 function saveEventData(gameId, data) {
+    const newEventPhase = getNewEventPhase(data.state.phase)
+
     const putParams = {
         TableName: eventTableName,
         Item: {
             gameId,
             timestamp: Date.now(),
-            data,
+            data: {
+                ...data,
+                state: {
+                    ...data.state,
+                    phase: newEventPhase,
+                }
+            },
         },
     }
 
     return documentClient.put(putParams).promise()
 }
 
-module.exports.postHitsEventHandler = async event => {
+function checkShouldSaveEvent(event) {
+    /*
+        Failed vote:
+        When going from vote -> proposed and no chancellor is selected
+    */
+    if (event.state.phase === 'propose') {
+        return event.state.previous_phase === 'vote'
+            && event.state.chancellor === null
+    }
+
+    /*
+        President Event:
+        Only save first president event to store what they were given (before they select card)
+    */
+    if (event.state.phase === 'president') {
+        return typeof event.state.to_discard === 'undefined'
+    }
+
+    /*
+        Chancellor Event:
+        Only save first president event to store what they were given (before they select card)
+    */
+   if (event.state.phase === 'chancellor') {
+       return typeof event.state.to_enact === 'undefined'
+   }
+
+    // Save all other events passed
+    return true
+}
+
+module.exports.postHitsEventHandler = async (event, context, callback) => {
     console.info(JSON.stringify(event))
     try {
         const hitsEvent = JSON.parse(event.body)
@@ -28,7 +78,24 @@ module.exports.postHitsEventHandler = async event => {
             throw new Error('No game id found')
         }
 
-        return saveEventData(gameId, hitsEvent)
+        const shouldSaveEvent = checkShouldSaveEvent(hitsEvent)
+
+        if (shouldSaveEvent) {
+            await saveEventData(gameId, hitsEvent)
+        }
+
+        const response = {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Origin': 'https://netgames.io',
+                'Access-Control-Allow-Credentials': true,
+            },
+            body: JSON.stringify({
+                saved: true
+            }),
+        };
+
+        callback(null, response);
     } catch (e) {
         console.error(e)
     }
