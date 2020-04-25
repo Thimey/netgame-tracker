9804eab229aac66c45cc6bff81e1ca556ab5ca97
Simon Angell <simon.angell307@gmail.com>
2020-04-25 10:57:27 +1000

    Save second last vote to get proposed chancellor

--- DataCollection/src/bookmarkletRecord.js
+++ DataCollection/src/bookmarkletRecord.js
@@ -8,18 +8,17 @@ javascript:(function(){
         'huddle',
         'intro',
         'identify',
-        'propose',
-        'vote'
+        'propose'
     ];
 
     function registerGameEvent(name) {
         window.netgames.socket.on(name, (eventData) => {
             const isNotUselessEvent = !uselessEvents.includes(eventData.state.phase);
-
+            const isSecondLastVote = eventData.state.phase === 'vote' &&  Object.values(eventData.state.votes).filter(v => v === null).length === 1;
             const isFailedVote = eventData.state.phase === 'propose' && eventData.state.previous_phase === 'vote' && eventData.state.chancellor === null;
 
             console.log(eventData);
-            if (isNotUselessEvent || isFailedVote) {
+            if (isNotUselessEvent || isFailedVote || isSecondLastVote) {
                 window.fetch('https://5uytvuys8d.execute-api.ap-southeast-2.amazonaws.com/dev/hits-event', {
                     method: 'POST',
                     headers: {
