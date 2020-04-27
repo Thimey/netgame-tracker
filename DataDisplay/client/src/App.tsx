74bad3c25730b90458a3a3e09ea1b05f33ffdedc
Simon Angell <simon.angell307@gmail.com>
2020-04-27 20:09:45 +1000

    Change to hashBrowser, add release notes

--- DataDisplay/client/src/App.tsx
+++ DataDisplay/client/src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { HashRouter as Router, Route, Switch } from 'react-router-dom'
 import { ThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
 import Typography from '@material-ui/core/Typography'
+import Divider from '@material-ui/core/Divider'
+import List from '@material-ui/core/List'
+import ListItem from '@material-ui/core/ListItem'
+import Card from '@material-ui/core/Card'
 
 import Header from './components/Header'
 import LiveView from './Views/LiveView'
@@ -23,10 +27,26 @@ const App: React.FC = () => (
         </Route>
         <Route path='/' exact>
           <div style={{ display: 'flex', flexDirection: 'column', margin: '16px' }}>
-              <Typography color='textPrimary' variant='h3'>Waiting for game to start...</Typography>
               <Typography color='textSecondary'>Ensure only ONE person is recording game.</Typography>
-              <br/>
-              <Typography color='textSecondary'>TODO: Load previous game state; currently refresh will purge past events</Typography>
+              <Typography color='textSecondary'>Type "/gameId" in url to view game</Typography>
+
+              <br />
+              <Divider />
+              <br />
+              <br />
+
+              <Card style={{ padding: '1rem' }}>
+                <Typography>Release notes (27/04/2020): v0.2.0</Typography>
+
+                <List>
+                  <ListItem>
+                    <Typography>Added previous game data load. Just type "/gameId" in the url</Typography>
+                  </ListItem>
+                  <ListItem>
+                    <Typography>Refreshing browser mid game should no longer lose the previous game states</Typography>
+                  </ListItem>
+                </List>
+              </Card>
           </div>
         </Route>
       </Switch>
